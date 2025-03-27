f59a299f49e1760563f05a97a96ddee94c798993
AnimPlayz98 <animplayz558@gmail.com>
2025-03-27 21:52:09 +0700

    Add files via upload
    ini tugas saya Naufal Fadlillah Aji dengan NIM 232410101082

---
+++ script.js
@@ -0,0 +1,9 @@
+// Tambahkan efek smooth scrolling saat mengklik menu navigasi
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+    anchor.addEventListener('click', function(event) {
+        event.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+        targetElement.scrollIntoView({ behavior: 'smooth' });
+    });
+});
